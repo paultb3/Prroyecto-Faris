7f3e49a86084fd0b74e7df40af38679d3f0d6287
paultb3 <paulbenancio3000@gmail.com>
2025-02-01 01:56:02 -0500

    Corrige el la vista del proyecto y agrega el archivo readme

---
+++ Readme.md
@@ -0,0 +1,30 @@
+# Adivina el Numero
+
+![Proyecto](../Prroyecto-Faris/src/img/proyecto.png)
+
+
+
+## Tabla de Contenidos
+
+- [Instalación](#instalación)
+- [Uso](#uso)
+- [Estructura del Proyecto](#estructura-del-proyecto)
+
+## Instalación
+
+
+```bash
+# Clonar el repositorio
+git clone https://github.com/FarisR2/Guees-The-Number.git
+
+# Navegar al directorio del proyecto
+cd nombre-del-repositorio
+
+# Instalar dependencias
+npm install
+
+# Correr el proyecto en local
+npm run dev
+
+# Ingresar con crtl + click derecho en windows
+![Proyecto](../Prroyecto-Faris/src/img/correr_proyecto.png)
--- src/main.js
+++ src/main.js
@@ -1,18 +1,11 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+
 
 import { buttonElement } from './random.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
+    <h1>Adivina tu numero!</h1>
     <div class="card">
         <label for="numberInput">Ingresa un número:</label>
         <input type="number" id="numberInput" placeholder="Escribe un número" />
